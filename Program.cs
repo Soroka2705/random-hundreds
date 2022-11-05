9816a00d066391b9df46efffae09615550c0ae93
soroka <sorokindv88@mail.ru>
2022-11-05 18:29:38 +0300

    bool prog1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int CutNumber(int number)
+﻿/*
+int CutNumber(int number)
 {
     int ed = number % 10;
     int sot = number / 100;
@@ -12,3 +13,23 @@ int num = new Random().Next(100, 1000);
 int result = CutNumber(num);
 Console.WriteLine($"new version of a number {num} is {result}");
 //345
+*/
+
+/*
+bool IsEven(int number)
+{
+if(number % 2 == 0)
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
+
+int num = new Random().Next(100, 1000);
+
+bool res = IsEven(num);
+Console.WriteLine($"number {num} is {res}");
+*/
