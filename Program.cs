939e4e128985342a06d0c06f72df14df557cb330
soroka <sorokindv88@mail.ru>
2022-11-05 18:05:35 +0300

    random hundreds

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int CutNumber(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+
+    number = sot * 10 + ed;
+    return number;
+}
+
+int num = new Random().Next(100, 1000);
+
+int result = CutNumber(num);
+Console.WriteLine($"new version of a number {num} is {result}");
+//345
